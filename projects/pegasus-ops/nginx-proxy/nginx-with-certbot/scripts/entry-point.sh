49f080efa174befb6d2727f61a7e3370ec1fa784
Sean Kleinjung <sean@thrashplay.com>
2020-03-29 10:30:14 -0500

    fix(sophia): Updated entry-point to copy config instead of editing in-place.
    Build #203
    Build-Meta: [skip ci]

--- projects/pegasus-ops/nginx-proxy/nginx-with-certbot/scripts/entry-point.sh
+++ projects/pegasus-ops/nginx-proxy/nginx-with-certbot/scripts/entry-point.sh
@@ -6,10 +6,18 @@ if [ ! -f "$CERTIFICATES" ]; then
   exit 1
 fi
 
+export PROXY_CONFIG=${PROXY_CONFIG_FILE:-/proxy.conf}
+if [ ! -f "$PROXY_CONFIG" ]; then
+  echo "PROXY_CONFIG_FILE does not exist: $PROXY_CONFIG"
+  exit 1
+fi
+
 if [ -z $CERTBOT_EMAIL ]; then
   echo "You must specify a CERTBOT_EMAIL environment variable, for urgent renewal and security notices."
   exit 1
 fi
+
+cp ${PROXY_CONFIG} /etc/nginx/conf.d/proxy.conf
 
 # start nginx
 nginx

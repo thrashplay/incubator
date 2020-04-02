83c5f6f90e198810a4d269dae1fe069d03fa72c2
Sean Kleinjung <sean@thrashplay.com>
2020-04-02 17:41:11 -0500

    feat: Completed initial version of Drone 'generate-versions' plugin.

--- packages/docker/generate-versions/scripts/entry-point.sh
+++ packages/docker/generate-versions/scripts/entry-point.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env sh
 
-if [ "$PLUGIN_USE_TLS" = "true" ]; then
-  VALID=true
-
-  if [ -z "$PLUGIN_DOCKER_CACERT" ]; then
-    VALID=false
-  fi
-  if [ -z "$PLUGIN_DOCKER_CERT" ]; then
-    VALID=false
-  fi
-  if [ -z "$PLUGIN_DOCKER_KEY" ]; then
-    VALID=false
-  fi
-
-  if [ "$VALID" != "true" ]; then
-    echo "If use_tls == true, you must supply the following: docker_cacert, docker_cert, and docker_key"
-    exit 1
-  fi
-
-  mkdir -p ~/.docker
-  echo "$PLUGIN_DOCKER_CACERT" > ~/.docker/ca.pem
-  echo "$PLUGIN_DOCKER_CERT" > ~/.docker/cert.pem
-  echo "$PLUGIN_DOCKER_KEY" > ~/.docker/key.pem
-
-  export DOCKER_TLS_VERIFY=1
+if [ -z "$PLUGIN_STACK_FILES" ]; then
+  echo "You must specify at least one 'stack_files' value."
+  exit 1
 fi
 
-export DOCKER_HOST=${PLUGIN_DOCKER_HOST:-tcp://localhost:2376}
-CONFIGURATION_FILE=${PLUGIN_CONFIGURATION_FILE:-swarm.management.yml}
+EXTRA_ARGS=
+if [ ! -z "$PLUGIN_OUTPUT" ]; then
+  EXTRA_ARGS="$EXTRA_ARGS --output $PLUGIN_OUTPUT"
+fi
 
-swm -start -f $CONFIGURATION_FILE
+INPUT_FILES=
+IFS=","
+for file in $PLUGIN_STACK_FILES
+do
+  INPUT_FILES=" --stack-file $"
+done
+
+set -x
+set -e
+
+npx @thrashplay/config-version-helper $INPUT_FILES $EXTRA_ARGS

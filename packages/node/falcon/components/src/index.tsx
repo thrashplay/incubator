c2be98108263dc3499ec7d7c7ffbc0f09d00eb9a
Sean Kleinjung <sean@thrashplay.com>
2020-05-13 08:40:23 -0500

    chore: Removed unused 'falcon' code.

--- packages/node/falcon/components/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Image, View } from 'react-native'
-
-export const HelloWorld = () => (
-  <View>
-    <Image source={require('./hello.jpg')} />
-  </View>
-)
--- packages/node/falcon/native/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native'
-
-import Root from './Root'
-
-// require('RCTNativeAppEventEmitter')
-AppRegistry.registerComponent('Falcon', () => Root)

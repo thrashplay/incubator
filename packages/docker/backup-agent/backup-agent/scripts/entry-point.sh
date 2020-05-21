266844ef4881241621bd42524e8523949e7e6165
Sean Kleinjung <sean@thrashplay.com>
2020-05-21 09:16:50 -0500

    fix: Fixed broken startup wait logic for backup-agent.

--- packages/docker/backup-agent/backup-agent/scripts/entry-point.sh
+++ packages/docker/backup-agent/backup-agent/scripts/entry-point.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-until /usr/local/bin/rclone_exec noop; do
-  >&2 echo "rclone service is unavailable - sleeping"
+ATTEMPTS=0
+RETRY_LIMIT=60
+
+echo "Waiting for rclone service..."
+until /usr/local/bin/rclone_exec rc/noop || ([ "$RETRY_LIMIT" -ne 0 ] && [ $ATTEMPTS -eq "$RETRY_LIMIT" ]); do
+  if [ "$RETRY_LIMIT" = "0" ]; then
+    >&2 echo Attempt $(( ++ATTEMPTS ))...
+  else
+    >&2 echo Attempt $(( ++ATTEMPTS )) of $RETRY_LIMIT...
+  fi
   sleep 1
 done
+
+if [ $ATTEMPTS -eq $RETRY_LIMIT ]; then
+   echo "Timed out waiting for rclone service."
+   exit 1
+else
+  echo "rclone service responded after $ATTEMPTS attempts."
+fi
 
 if [[ -n "${RCLONE_OPTIONS_FILE}" ]]; then
   OPTIONS_CONTENT=$(<${RCLONE_OPTIONS_FILE})

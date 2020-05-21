94da442eb917a2fc0e4fe1d4abaaf0fb25ca5a4c
Sean Kleinjung <sean@thrashplay.com>
2020-05-21 09:06:21 -0500

    fix: Added guard to prevent backup-agent from running before rclone service is ready.

--- packages/docker/backup-agent/backup-agent/scripts/entry-point.sh
+++ packages/docker/backup-agent/backup-agent/scripts/entry-point.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+until /usr/local/bin/rclone_exec noop; do
+  >&2 echo "rclone service is unavailable - sleeping"
+  sleep 1
+done
+
 if [[ -n "${RCLONE_OPTIONS_FILE}" ]]; then
   OPTIONS_CONTENT=$(<${RCLONE_OPTIONS_FILE})
   echo "Setting rclone options: ${OPTIONS_CONTENT}"

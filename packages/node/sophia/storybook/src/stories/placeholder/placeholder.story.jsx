89eb5432f89cc7fc4e9305696d522078fdf8e866
Sean Kleinjung <sean@thrashplay.com>
2020-09-18 04:41:22 +0000

    feat: add remote container development support

---
+++ packages/node/sophia/storybook/src/stories/placeholder/placeholder.story.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MapView, HexGrid } from '@thrashplay/hexmarch-ui'
+import { Text, View } from 'react-native'
+
+import { storiesOf } from '../../stories-of'
+
+storiesOf('Placeholder', module)
+  .add('default', () => (
+    <Text>Hello, world!</Text>
+  ))

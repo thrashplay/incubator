a8c96b5c84afab3dab5b3aa0de0f9ec3af4c9152
Sean Kleinjung <sean@thrashplay.com>
2020-10-28 18:36:32 +0000

    feat(@thrashplay/gemstone-engine): Created Rules model package.

---
+++ packages/node/gemstone/engine/src/model/rules/__fixtures__/index.ts
@@ -0,0 +1,52 @@
+export const MovementModeFixtures = {
+  Crawl: {
+    id: 'crawl',
+    name: 'Four Legs in the Morning',
+    multiplier: 0.1,
+  },
+  Walk: {
+    id: 'walk',
+    name: 'Two Legs in the Afternoon',
+    multiplier: 1,
+  },
+  WithCane: {
+    id: 'with-cane',
+    name: 'Three Legs in the Evening',
+    multiplier: 0.5,
+  },
+}
+
+export const RulesStateFixtures = {
+  Default: {
+    movement: {
+      defaultMode: 'hustle',
+      modes: {
+        cautious: {
+          id: 'cautious',
+          name: 'Cautious',
+          multiplier: 0.1,
+        },
+        hustle: {
+          id: 'hustle',
+          name: 'Hustle',
+          multiplier: 1,
+        },
+        run: {
+          id: 'run',
+          name: 'Run',
+          multiplier: 2,
+        },
+      },
+    },
+  },
+  RiddleOfTheSphinx: {
+    movement: {
+      defaultMode: 'walk',
+      modes: {
+        crawl: MovementModeFixtures.Crawl,
+        walk: MovementModeFixtures.Walk,
+        'with-cane': MovementModeFixtures.WithCane,
+      },
+    },
+  },
+}
---
+++ packages/node/gemstone/engine/src/model/rules/actions.ts
@@ -0,0 +1,4 @@
+export const RulesActions = {
+}
+
+export type RulesAction = { type: string }

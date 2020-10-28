a7045f39c417a65b679b117a0f4b5d4e8b47423d
Sean Kleinjung <sean@thrashplay.com>
2020-10-28 18:43:13 +0000

    feat(@thrashplay/gemstone-engine): Added top-level model files and APIs.

---
+++ packages/node/gemstone/engine/src/model/index.ts
@@ -0,0 +1,15 @@
+import * as CharacterModel from './character'
+import * as RulesModel from './rules'
+import * as SceneModel from './scene'
+
+export * from './action'
+export * from './common'
+export * from './character'
+export * from './reducer'
+export * from './rules'
+export * from './scene'
+export * from './selectors'
+export * from './state'
+export * from './types'
+
+export { CharacterModel, RulesModel, SceneModel }

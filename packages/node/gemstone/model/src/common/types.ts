ce4669c693b1f469f4bd53b7a15bb8588eece5b6
Sean Kleinjung <sean@thrashplay.com>
2020-10-29 20:42:35 +0000

    fix(@thrashplay/gemstone-model): Fixed bug allowing 'NaN' positions to be set for an actor.

--- packages/node/gemstone/model/src/common/types.ts
+++ packages/node/gemstone/model/src/common/types.ts
@@ -1,6 +1,13 @@
 /** simple type definitions used by the various state interfaces */
 
+import { isFinite } from 'lodash/fp'
+
 export interface Point {
   x: number
   y: number
+}
+
+export const isValidPoint = (point: any): point is Point => {
+  const { x, y } = point ?? {}
+  return isFinite(x) && isFinite(y)
 }

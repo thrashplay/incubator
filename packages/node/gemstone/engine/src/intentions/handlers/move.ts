c9f2b339e013860748632e9d897582d4a4a751bc
Sean Kleinjung <sean@thrashplay.com>
2020-10-29 21:33:15 +0000

    wip - refactoring movement to facilitate approaching, STASHING

--- packages/node/gemstone/engine/src/intentions/handlers/move.ts
+++ packages/node/gemstone/engine/src/intentions/handlers/move.ts
@@ -1,6 +1,8 @@
-import { ActorStatus, getBaseSpeed, Point, SimulationActions } from '@thrashplay/gemstone-model'
+import { isEmpty } from 'lodash/fp'
 
-import { calculateDistance, getNewPosition } from '../../simulation'
+import { ActorStatus, Point, SimulationActions } from '@thrashplay/gemstone-model'
+
+import { approachLocation, calculateDistance } from '../../simulation'
 import { GameState } from '../../state'
 import { createIntention } from '../intentions'
 import { SimulationContext } from '../types'
@@ -8,25 +10,21 @@ import { SimulationContext } from '../types'
 const ARRIVAL_DISTANCE = 3
 
 export const moveTowardsDestination = (actor: ActorStatus, destination: Point, state: GameState) => {
-  const speed = getBaseSpeed(state, { characterId: actor.id })
-  const newPosition = getNewPosition(actor.position, destination, speed, 1)
-
-  return SimulationActions.moved({
-    characterId: actor.id,
-    position: newPosition,
-  })
+  return approachLocation(actor.id, destination, 0)(state)
 }
 
 /** handles the outcome of a move intention, once time has advanced to the endSegment */
-export const move = (actor: ActorStatus, context: SimulationContext<GameState>, destination: Point) => {
-  const moveAction = moveTowardsDestination(actor, destination, context.state)
+export const move = (actor: ActorStatus, { state }: SimulationContext<GameState>, destination: Point) => {
+  const moveAction = approachLocation(actor.id, destination, 0)(state)
 
   const beginIdlingAction = SimulationActions.intentionDeclared({
     characterId: actor.id,
     intention: createIntention('idle'),
   })
 
-  return (calculateDistance(moveAction.payload.position, destination) < ARRIVAL_DISTANCE)
-    ? [moveAction, beginIdlingAction]
-    : moveAction
+  return isEmpty(moveAction)
+    ? []
+    : (calculateDistance(moveAction[0].payload.position, destination) < ARRIVAL_DISTANCE)
+      ? [...moveAction, beginIdlingAction]
+      : moveAction
 }

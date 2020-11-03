35efeedcaef1bbf8ec423e61f348616789f72980
Sean Kleinjung <sean@thrashplay.com>
2020-11-03 13:44:47 +0000

    feat(@thrashplay/fp): Added toDictionary function for reducing arrays to Records.

--- packages/node/thrashlib/fp/src/to-dictionary.ts
+++ packages/node/thrashlib/fp/src/to-dictionary.ts
@@ -1,13 +1,15 @@
 import { get, reduce } from 'lodash/fp'
 
-type SourceObject = Partial<Record<string, any>>
-export const toDictionary = <TObject extends SourceObject = SourceObject>(
-  keyProp: keyof TObject
-) => (input: TObject[]): Partial<Record<string, TObject>> => {
-  const reduceToDictionary = (result: Partial<Record<string, TObject>>, value: TObject) => ({
-    ...result,
-    [get(keyProp)(value)]: value,
-  })
+export const toDictionary = (keyProp: string) => <
+  TObject extends unknown = any
+>(input: TObject[]) => {
+  const reduceToDictionary = (result: Partial<Record<string, TObject>>, value: TObject) => {
+    const key: string = get(keyProp)(value)
+    return {
+      ...result,
+      [key]: value,
+    }
+  }
 
   return reduce(reduceToDictionary)({ })(input)
 }

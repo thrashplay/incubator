973bc3e425095fe4ea6fccafc2d7a9416abbb2b3
Sean Kleinjung <sean@thrashplay.com>
2020-11-24 01:25:25 +0000

    Updated Facet interface to allow facets to specify their default state.

--- packages/node/gemstone/engine/src/facet/extend.ts
+++ packages/node/gemstone/engine/src/facet/extend.ts
@@ -5,9 +5,10 @@ import { Entity } from '../entity'
 import { Facet } from './facet'
 
 /** Extends an entity by adding the specified facet to it. */
-export const extend = curry((facet: Facet, entity: Entity) => {
+export const extend = curry((facet: Facet<any>, entity: Entity) => {
   return {
     ...entity,
+    ...(facet.defaultState ?? {}),
     facets: [facet.id, ...entity.facets],
   }
 })
--- packages/node/gemstone/engine/src/facet/facet.ts
+++ packages/node/gemstone/engine/src/facet/facet.ts
@@ -1,10 +1,15 @@
+import { Dictionary } from '@thrashplay/gemstone-model'
+
 import { ActionHandler } from '../action'
 
 /** Facets provide a cluster of related state and behaviors for an entity, and can be combined dynamically. */
-export type Facet = {
+export type Facet<TState extends Dictionary<string, any> = never> = {
   /** Unique identifier for this facet. */
   id: string
 
   /** Set of default action handlers added to entities with this facet. */
   defaultActionHandlers: readonly ActionHandler[]
-}
+} & ([TState] extends [never] ? unknown : {
+  /** Default state to add to an entity when it first gains this facet. If undefined, no state is added. */
+  defaultState: TState
+})
